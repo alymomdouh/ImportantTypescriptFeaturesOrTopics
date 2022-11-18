3919cdc5275cd46b249ec68796c945788d6d9e71
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2022-11-18 22:49:23 +0200

    Utility types Required

--- AdvancedTypeScript.ts
+++ AdvancedTypeScript.ts
@@ -127,6 +127,22 @@ interface MovieCharacter {
 }
 function registerCharacter(character: Partial<MovieCharacter>) { }
 // 👍
-registerCharacter({firstname: 'Frodo',});
+registerCharacter({ firstname: 'Frodo', });
 // 👍
-registerCharacter({firstname: 'Frodo',name: 'Baggins',});
+registerCharacter({ firstname: 'Frodo', name: 'Baggins', });
+
+/**
+ * Required
+Required does the opposite of Partial. It takes an existing interface with optional properties
+ and transforms it into a type where all properties are required.
+ */
+interface MovieCharacter {
+    firstname?: string;
+    name?: string;
+    movie?: string;
+}
+function hireActor(character: Required<MovieCharacter>) { }
+// 👍
+hireActor({firstname: 'Frodo', name: 'Baggins', movie: 'The Lord of the Rings'});
+// 👎
+hireActor({ firstname: 'Frodo', name: 'Baggins'});
